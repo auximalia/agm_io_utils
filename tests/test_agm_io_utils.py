c5c22d4268dff76779d3f3ce579a2de60bbe7b7c
Matthias Riedel <mriedel@t-systems.com>
2021-08-09 17:37:30 +0200

    add test cases

--- tests/test_agm_io_utils.py
+++ tests/test_agm_io_utils.py
@@ -1,7 +1,22 @@
+import os
 from agm_io_utils.io_utils import get_all_files_recursively_from_path
+from agm_io_utils.io_utils import check_path_and_return_new_file_name_with_postfix
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    my_path = r"C:\Users\mtri\Documents\test"
+
+    def test_check_path_and_return_new_file_name_with_postfix(self):
+        res = check_path_and_return_new_file_name_with_postfix("testfile.test")
+        self.assertEqual("", res)
+
+    def test_get_all_files_recursively_from_path(self):
+        res = get_all_files_recursively_from_path(self.my_path)
+        self.assertTrue(res)
+        for f in res:
+            print(f)
+
 
 if __name__ == '__main__':
-    my_path = r"C:\Users\mtri\Documents\test"
-    result = get_all_files_recursively_from_path(my_path)
-    for r in result:
-        print(r)
+    unittest.main()
