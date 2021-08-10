b7188336569c071d36df72381baf530b318c1618
auximalia <git@bureau-agamemnon.de>
2021-08-10 20:36:15 +0200

    Fixed miss matches and examples

--- tests/test_it.py
+++ tests/test_it.py
@@ -2,7 +2,7 @@ from agm_io_utils.io_utils import get_all_files_recursively_from_path
 from agm_io_utils.io_utils import get_all_files_recursively_from_path_with_statistics
 
 if __name__ == '__main__':
-    my_path = r"E:\music_imported_by_python"
+    my_path = r"E:\music"
     result, stats = get_all_files_recursively_from_path_with_statistics(my_path,
                                                                         not_allowed_in_files_name_list=["Heaven", "Rain"],
                                                                         not_allowed_in_path_string_list=["Download", "Wall"],
