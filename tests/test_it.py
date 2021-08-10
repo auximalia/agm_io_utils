60e06922393481d099a54ca5abe23a52176983c3
auximalia <git@bureau-agamemnon.de>
2021-08-10 21:22:10 +0200

    adapt example

--- tests/test_it.py
+++ tests/test_it.py
@@ -2,7 +2,7 @@ from agm_io_utils.io_utils import get_all_files_recursively_from_path
 from agm_io_utils.io_utils import get_all_files_recursively_from_path_with_statistics
 
 if __name__ == '__main__':
-    my_path = r"E:\music"
+    my_path = r"C:\Users\micro\OneDrive\Dokumente\test"
     result, stats = get_all_files_recursively_from_path_with_statistics(my_path,
                                                                         not_allowed_in_files_name_list=["Heaven", "Rain"],
                                                                         not_allowed_in_path_string_list=["Download", "Wall"],
